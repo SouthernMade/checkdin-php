8784779c9314b4c07fdfb5dbc97cdade1cd73c72
Laust Rud Jacobsen <laust@object.io>
2013-06-03 15:37:10 +0200

    Docs

--- inc/checkdin_config.php
+++ inc/checkdin_config.php
@@ -4,12 +4,13 @@ class CheckdinConfigError extends Exception { }
 class CheckdinConfig {
   const VERSION = '0.0.1';
 
-  // API Credentials
+  // API Credentials. Replace the TODO string with your actual value
   function clientID() {
     $clientId = 'TODO';
     return $this->ensureValueSet($clientId);
   }
 
+  // API Credentials. Replace the TODO string with your actual value
   function clientSecret() {
     $clientSecret = 'TODO';
     return $this->ensureValueSet($clientSecret);
@@ -20,6 +21,7 @@ class CheckdinConfig {
     return 'https://app.checkd.in/api/v1';
   }
 
+  // Internal bouncer method to ensure the value is configured
   function ensureValueSet($value) {
     if ($value == 'TODO') {
       throw new CheckdinConfigError("TODO: Open checkdin_config.php and insert your credentials, please");
