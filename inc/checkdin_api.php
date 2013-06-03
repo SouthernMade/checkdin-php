fa07162d468aff24a2662d77ca570b812c965d6c
Laust Rud Jacobsen <laust@object.io>
2013-06-03 16:37:18 +0200

    CheckdinApi can take a config argument, or create an instance itself

--- inc/checkdin_api.php
+++ inc/checkdin_api.php
@@ -1,5 +1,21 @@
 <?php
+require_once 'checkdin_config.php';
+
 class CheckdinApi {
   const VERSION = '0.0.1';
+
+  private $config;
+
+  function __construct($config = NULL) {
+    if ($config) {
+      $this->config = $config;
+    } else {
+      $this->config = new CheckdinConfig();
+    }
+  }
+
+  function apiUrl() {
+    return $this->config->apiUrl();
+  }
 }
 ?>
--- test/checkdin_api_test.php
+++ test/checkdin_api_test.php
@@ -4,10 +4,18 @@ require_once 'inc/checkdin_api.php';
 class CheckdinApiTest {
   function all_tests() {
     self::test_version();
+    self::test_default_config_instantiation();
   }
 
   function test_version() {
     assert_equal(CheckdinApi::VERSION, '0.0.1');
+  }
+
+  function test_default_config_instantiation() {
+    $instance = new CheckdinApi();
+    $actual = $instance->apiUrl();
+    $expected = 'https://app.checkd.in/api/v1';
+    assert_equal($actual, $expected);
   }
 }
 
